3a8d0d68316515fea061f8da37630798b297f391
proscrite <pablogfr94@gmail.com>
2025-08-18 11:31:50 +0300

    Refactor peak extraction and histogram plotting functions in wfm2spectra.py

--- scripts/wfm2spectra.py
+++ scripts/wfm2spectra.py
@@ -1,62 +1,32 @@
 from glob import glob
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
+
 # from .wfm2read_fast import wfm2read
 from RaTag.scripts.wfm2read_fast import wfm2read
 
-def get_all_peaks(path):
-    """Function to read waveforms and plot histogram of peaks."""
-    
-    files = glob(path + '/*.wfm')
+def get_peak_volts(file_path):
+    """
+    Function to read all waveform files in a directory and extract peaks.
+    :param file_path: Path to the directory containing waveform files.
+    :return: List of peaks found in the waveform files.
+    """
+    y, t, info, ind_over, ind_under = wfm2read(file_path, step=0.001)
 
-    peaks = []
-    for f in files:
-        y, t, peak, info, ind_over, ind_under = wfm2read(f)
-        peaks.append(peak)
-    peaks = np.array(peaks)
-    return peaks
+    peaks, props = find_peaks(y, height=1, distance=1, prominence=1)
+    volts = y[peaks]
+    return volts
 
-def histogram_peaks(peaks):
+def histogram_voltages(volts):
     """Function to plot histogram of peaks."""
-    plt.hist(peaks, bins=4, edgecolor='black')
+    plt.hist(volts, bins=100, edgecolor='black')
     plt.xlabel('Peak Value')
     plt.ylabel('Frequency')
     plt.title('Histogram of Peaks from Waveform Files')
     plt.show()
 
-
-def get_total_gain(dial, coarse_gain = 50):
-    Gmin = 0.5
-    Gmax = 1.5
-    dmin = 0
-    dmax = 100
-    Gfine = Gmin + (Gmax - Gmin) * (dial - dmin) / (dmax - dmin)
-    Gtotal = Gfine * coarse_gain
-    return Gtotal
-
-def channel_to_voltage(channel, fine_gain = 63, coarse_gain = 50):
-    """
-    Convert a channel number to voltage and energy peak.
-    :param channel: Channel number (0-2047)
-    :param fine_gain: Fine gain setting (0-63)
-    :param coarse_gain: Coarse gain setting (default 50)
-    :return: Tuple of voltage in mV and energy peak in eV
-    """
-    if not (0 <= channel < 2048):
-        raise ValueError("Channel must be between 0 and 2047")
-    
-    gain_amp = get_total_gain(fine_gain, coarse_gain)
-    gain_preamp = 45 # mV / MeV preamp gain
-
-    V = channel * 10 / 2048 # V
-    V_mV = V * 1000 # mV
-
-    E_peak = V_mV / (gain_amp * gain_preamp) # MeV
-
-    print(f"V = {V:.2f} V")
-    print(f"E_peak = {E_peak:.2f} MeV")
-    return V_mV, E_peak
 
 if __name__ == "__main__":
     
@@ -66,7 +36,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     path = sys.argv[1]
-    peaks = get_all_peaks(path)
+    peaks = get_peak_volts(path)
     print(f"Found {len(peaks)} peaks in {path}")
     print(f"Peaks: {peaks}")
-    histogram_peaks(peaks)
+    histogram_voltages(peaks)
