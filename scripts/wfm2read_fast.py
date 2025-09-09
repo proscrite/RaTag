bd23cbca2297056ee39fbfc2650c0a2276fb3f3b
proscrite <pablogfr94@gmail.com>
2025-09-09 17:31:27 +0200

    Add fastframes reading in wfm2read_fast

--- scripts/wfm2read_fast.py
+++ scripts/wfm2read_fast.py
@@ -143,15 +143,16 @@ def wfm2read(filename, datapoints=None, step=1, startind=0, verbose=False):
         Nframes = info['N']
         pts_per_frame = nop_all if Nframes > 0 else nop_all
 
-        print(f'len V = {nop_all}, Nframes = {Nframes}, pts_per_frame = {pts_per_frame}')
+        
         if  Nframes > 1:
             # interpret fractional step as "read all frames"
             datapoints = nop_all * Nframes
-        # if datapoints is None:
-        #     datapoints = int(nop // step)
-        # else:
-        #     if datapoints > nop:
-        #         datapoints = int(nop // step)
+        
+        elif datapoints is None:
+            datapoints = int(nop // step)
+        else:
+            if datapoints > nop:
+                datapoints = int(nop // step)
 
         if verbose:
             print(f"Reading {datapoints} data points from {filename} starting at index {startind} with step {step}")
@@ -164,7 +165,7 @@ def wfm2read(filename, datapoints=None, step=1, startind=0, verbose=False):
         
         if Nframes > 1:
             points_per_frame = datapoints // Nframes
-            print(f"Reshaping V to ({Nframes}, {points_per_frame})")
+            # print(f"Reshaping V to ({Nframes}, {points_per_frame})")
             y = y.reshape((Nframes, -1))
             t = t[:points_per_frame]
 
