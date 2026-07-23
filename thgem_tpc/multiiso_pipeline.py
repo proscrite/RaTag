ebb039045557406849564162d6a268ba78e4a34a
Pablo <pablogfr94@gmail.com>
2026-07-23 11:28:43 +0300

    Updated yaml files

--- thgem_tpc/multiiso_pipeline.py
+++ thgem_tpc/multiiso_pipeline.py
@@ -19,5 +19,5 @@ def pipeline_multi_isotope(run: Run, config: dict = None) -> Run:
         # 3. Plot: same as in single-isotope recoil workflow
         iso_run = map_recoil_plots(iso_run, config=fit_config)
     
-    run = map_multiiso_s2_vs_field(bare_run=run, spawned_runs=multi_run_dict)
+    run = map_multiiso_s2_vs_field(run, spawned_runs=multi_run_dict)
     return run
