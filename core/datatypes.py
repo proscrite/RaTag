Fix UID overflow for FastFrames size over 64 (now maximum 10_000 frames have actual UIDs), add uid_key_reset script for alphas

core/datatypes.py
@@ -31,13 +31,13 @@ class Waveform:
     
     @property
     def uid(self) -> int:
-        return self.file_seq * 64 + self.frame_idx
+        return self.file_seq * 10_000 + self.frame_idx
     
     @property
     def uids(self) -> np.ndarray:
         """Return array of UIDs for each frame (for FastFrame) or single UID for single-frame."""
         if self.ff and self.nframes > 1:
-            return  self.file_seq * 64 + np.arange(self.nframes)
+            return  self.file_seq * 10_000 + np.arange(self.nframes)
         else:
             return np.array([self.uid])
 

core/uid_utils.py
@@ -22,12 +22,11 @@ def parse_file_seq_from_name(fname: str) -> int:
     raise ValueError(f"Cannot parse file_seq from filename {base}")
 
 def make_uid(file_seq: int, frame_idx: int) -> int:
-    # frame_idx expected 0..48
-    return int(file_seq) * 64 + int(frame_idx)
+    return int(file_seq) * 10_000 + int(frame_idx)
 
 def decode_uid(uid: int):
-    file_seq = uid // 64
-    frame_idx = uid % 64
+    file_seq = uid // 10_000
+    frame_idx = uid % 10_000
     return file_seq, frame_idx
 
 def generate_all_uids_for_set(set_pmt) -> np.ndarray: