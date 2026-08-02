781bcd88289e98a43eb7fa4889bca6cc93d92b75
Pablo <pablogfr94@gmail.com>
2026-08-02 13:56:51 +0300

    Refactor alpha_pipeline to include FitConfig and streamline decoupled force parameter usage from FitConfig, updated lmfit and scipy version installation

--- alphas/alphas_pipeline.py
+++ alphas/alphas_pipeline.py
@@ -1,7 +1,7 @@
 # RaTag/alphas/alpha_pipeline.py
 from typing import Optional
 from RaTag.core.datatypes import Run
-from RaTag.core.config import AlphaCalibrationConfig
+from RaTag.core.config import AlphaCalibrationConfig, FitConfig
 
 
 from RaTag.alphas.alphas_workflow import map_alpha_events
@@ -28,17 +28,17 @@ def pipeline_alpha_calibration(run: Run, config: dict = None) -> Run:
     alpha_config = config.get('energy_mapping', AlphaCalibrationConfig())
     alpha_config = AlphaCalibrationConfig(**{k: v for k, v in alpha_config.items() if hasattr(AlphaCalibrationConfig, k)})
     
-    force = alpha_config.force
     max_frames = alpha_config.max_frames
-    
+    fit_config = config.get('fit_config', FitConfig())
+    fit_config = FitConfig(**{k: v for k, v in fit_config.items() if hasattr(FitConfig, k)})
     
     # 1. Extraction
-    run = map_alpha_events(run, max_frames=max_frames, config=alpha_config, force=force)
+    run = map_alpha_events(run, max_frames=max_frames, config=alpha_config, force=alpha_config.force)
     
     # 2. Pure Math & Calibration (Saves flat metadata and fits.json)
-    run = map_alpha_calibrations(run, energy_range=alpha_config.energy_range, force=force)
+    run = map_alpha_calibrations(run, energy_range=alpha_config.energy_range, force=fit_config.force)
     
     # 3. Presentation (Saves plots)
-    run = map_alpha_plots(run, force=force)
+    run = map_alpha_plots(run, force=fit_config.force)
     
     return run
